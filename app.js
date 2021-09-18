e33694dac8fe1e4b3493dd9f9c38eab0dfc98b61
DeathRaven1051 <67112322+DeathRaven1051@users.noreply.github.com>
2021-09-18 21:12:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 const dbURI = 'mongodb+srv://deathraven:deathraven@cluster0.vgbbl.mongodb.net/node-auth';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(3000, () => {
+   .then((result) => app.listen(process.env.PORT || 3000, () => {
         console.log("SERVER STARTED");
     }))
     .catch((err) => console.log(err));
